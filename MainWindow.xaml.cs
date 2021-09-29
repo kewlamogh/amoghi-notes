a0591f122f97d722c871674c27103b61189c118d
DaCool1 <86981743+kewlamogh@users.noreply.github.com>
2021-09-29 08:00:42 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Linq;
+
+namespace amoghi_notes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void ClearClick(object sender, RoutedEventArgs e)
+        {
+            noteText.Text = "";
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (string i in Persistance.GetAll())
+            {
+                if (i.Trim() != "") notes.Items.Add(i.Trim());
+            }
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string was = Alert.Text;
+            Alert.Text = "";
+
+            Persistance.Save(notes.Items);
+
+            if (noteText.Text == "")
+            {
+                if (was == "[EDIT]") { _ = MessageBox.Show("Deleted!"); };
+                return;
+            }
+
+
+            TextBox x = noteText;
+            notes.Items.Add(x.Text);
+            x.Text = "";
+
+            Persistance.Save(notes.Items);
+        }
+
+        private void Notes_DoubleClick(object sender, EventArgs e)
+        {
+
+            noteText.Text = notes.Items[notes.SelectedIndex].ToString();
+            notes.Items.Remove(notes.Items[notes.SelectedIndex]);
+            Alert.Text = "[EDIT]";
+
+            Persistance.Save(notes.Items);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Persistance.Write( new List<string>() );
+            MessageBox.Show("Deleted!");
+            notes.Items.Clear();
+        }
+
+        private void y_Click(object sender, RoutedEventArgs e)
+        {
+            IEnumerable<string> x = from y in Persistance.GetAll()
+                                    where y.Contains(query.Text)
+                                    select y;
+            results.Items.Clear();
+            foreach (var i in x)
+            {
+                results.Items.Add(i);
+            }
+        }
+    }
+}
---
+++ Persistance.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+public class Persistance
+{
+    public static string[] GetAll()
+    {
+        return File.ReadAllText(@"C:\Users\amogh\source\repos\amoghi-notes\Persistor.txt").Split("\n");
+    }
+    public static void Write(List<string> x)
+    {
+        File.WriteAllLines(@"C:\Users\amogh\source\repos\amoghi-notes\Persistor.txt", x);
+    }
+    public static void Save(ItemCollection o)
+    {
+        List<string> props = new();
+        foreach (object i in o) props.Add(i.ToString().Trim());
+        Write(props);
+    }
+}
